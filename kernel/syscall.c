5e6dbbe42da4d2c716245c935a393a75bc6f654c
Hua Zhangzhao <jeremyhua@foxmail.com>
2022-07-01 15:23:18 +0800

    lab3-2 完成

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -76,8 +76,17 @@ ssize_t sys_user_yield() {
   // hint: the functionality of yield is to give up the processor. therefore,
   // we should set the status of currently running process to READY, insert it in
   // the rear of ready queue, and finally, schedule a READY process to run.
-  panic( "You need to implement the yield syscall in lab3_2.\n" );
+//  panic( "You need to implement the yield syscall in lab3_2.\n" );
+/*
+ * 进程释放CPU的动作应该是：
+ * 1-将当前进程置为就绪状态（READY）；
+ * 2-将当前进程加入到就绪队列的队尾；
+ * 3-转进程调度。
+ */
 
+    current->status = READY;
+    insert_to_ready_queue( current );
+    schedule();
   return 0;
 }
 
--- user/app_yield.c
+++ user/app_yield.c
@@ -30,3 +30,41 @@ int main(void) {
   exit(0);
   return 0;
 }
+
+/*
+ * 和lab3_1一样，以上的应用程序通过fork系统调用创建了一个子进程，接下来，父进程和子进程都进入了一个很长的循环。
+ * 在循环中，无论是父进程还是子进程，在循环的次数是10000的整数倍时，除了打印信息外都调用了yield()函数，来释放自己的执行权（即CPU）。
+ */
+
+//spike obj/riscv-pke obj/app_yield
+//        In m_start, hartid:0
+//HTIF is available!
+//(Emulated) memory size: 2048 MB
+//        Enter supervisor mode...
+//PKE kernel start 0x0000000080000000, PKE kernel end: 0x0000000080009000, PKE kernel size: 0x0000000000009000 .
+//free physical memory address: [0x0000000080009000, 0x0000000087ffffff]
+//kernel memory manager is initializing ...
+//        KERN_BASE 0x0000000080000000
+//physical address of _etext is: 0x0000000080005000
+//kernel page table is on
+//        Switch to user mode...
+//        in alloc_proc. user frame 0x0000000087fbc000, user stack 0x000000007ffff000, user kstack 0x0000000087fbb000
+//User application is loading.
+//Application: obj/app_yield
+//        CODE_SEGMENT added at mapped info offset:3
+//Application program entry point (virtual address): 0x000000000001017c
+//going to insert process 0 to ready queue.
+//going to schedule process 0 to run.
+//User call fork.
+//will fork a child from parent 0.
+//in alloc_proc. user frame 0x0000000087faf000, user stack 0x000000007ffff000, user kstack 0x0000000087fae000
+//going to insert process 1 to ready queue.
+//Parent: Hello world!
+//Parent running 0
+//You need to implement the yield syscall in lab3_2.
+
+/*
+ * (1)分析
+ * ：完善yield系统调用，实现进程执行过程中的主动释放CPU的动作 ==>kernel/syscall.c
+ *
+ */
